634c0624ac19274da7abbc86776e6e89f00e1d7b
yukidaruma <yukidaruma@users.noreply.github.com>
2022-10-29 01:27:41 +0900

    feat: Use FontAwesome

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,11 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faTwitter);
+library.add(faYoutube);
+
+createApp(App).component('fa', FontAwesomeIcon).mount('#app');

800bd1118cb23f1ffbbc33e3741f4a1b47c39f3e
yukidaruma <yukidaruma@users.noreply.github.com>
2022-10-29 03:28:11 +0900

    feat: Add loading indicator to tweet embed

--- src/pages/PlayerPage/PlayerPage.vue
+++ src/pages/PlayerPage/PlayerPage.vue
@@ -3,6 +3,7 @@ import { useRoute, useRouter } from 'vue-router';
 import Tweet from 'vue-tweet';
 
 import players from '@/players';
+import { ref } from 'vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -11,6 +12,8 @@ const player = players.find((player) => player.name === route.params.player)!;
 if (!player) {
   router.push('/');
 }
+
+const isLoadingTweet = ref(true);
 </script>
 
 <template>
@@ -18,9 +21,21 @@ if (!player) {
   <div class="text-center">
     <h2 class="text-white text-2xl mt-8">{{ player.name }}</h2>
     <p class="mt-4">{{ player.bio }}</p>
+    <p class="mt-8">{{ player.bio }}</p>
 
     <div v-if="player.tweet" class="tweet-container mt-8">
-      <Tweet :width="500" :tweet-url="player.tweet" />
+      <div
+        v-if="isLoadingTweet"
+        role="status"
+        class="flex justify-center items-center text-4xl h-60"
+      >
+        <Loading :size="16" />
+      </div>
+      <Tweet
+        @tweet-load-success="isLoadingTweet = false"
+        :width="500"
+        :tweet-url="player.tweet"
+      />
     </div>
   </div>
 </template>

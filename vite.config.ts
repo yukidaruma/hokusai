4168efc8280260ccb87e96ac2ab4c27e7470ab6b
yukidaruma <yukidaruma@users.noreply.github.com>
2022-10-29 02:00:03 +0900

    feat: Use absolute import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tsConfigPaths({
+      loose: true,
+    }),
+  ],
 });
